4ec9e2aa8d55caa1196b5159bafd51659abd8fbe
MSMazaya <msulthanmazaya@gmail.com>
2022-08-26 19:01:19 +0700

    Fix servo issue

--- server/models/servo.py
+++ server/models/servo.py
@@ -1,11 +1,19 @@
-
+port RPi.GPIO as GPIO
 from firebase_admin import threading
 
 
 class Servo:
-    def __init__(self):
+    def __init__(self, pin):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+
         self.pointsData = []
         self.callback_done = threading.Event()
+
+        GPIO.setup(pin, GPIO.OUT)
+
+        self.pwm_channel = GPIO.PWM(pin, 50)
+        self.pwm_channel.start(0)
 
     def getPointsData(self, col_snapshot, changes, read_time):
         newData= []
@@ -16,7 +24,10 @@ class Servo:
         self.callback_done.set()
 
     def isReady(self):
-        return len(self.pointsData) 
+        return len(self.pointsData)
 
     def searchForBall(self):
         pass
+
+    def goToAngle(self, angle):
+        self.pwm_channel.ChangeDutyCycle(2+(float(angle)/18))
