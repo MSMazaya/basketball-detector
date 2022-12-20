94a87edcbd9facabcb45ceca8b43d3bc7d8fd294
MSMazaya <msulthanmazaya@gmail.com>
2022-12-20 14:46:11 +0700

    make sure routine to get ball is the same as previous one

--- server/main.py
+++ server/main.py
@@ -1,16 +1,67 @@
+from io import StringIO, BytesIO
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+from socketserver import ThreadingMixIn
+import threading
+from PIL import Image
 import cv2
 from models.db import DB
 from models.camera import Camera
 from models.utils import getIP
-#
-# db = DB()
-# db.changeIP(getIP())
-#
+
+db = DB()
+db.changeIP(getIP())
+
+
 def createCamera():
     bufferThickness = 30
     orangeLower = (10, 200, 20)
     orangeUpper = (15, 255, 255)
     return Camera(orangeLower, orangeUpper, bufferThickness)
+
+
+'''
+	Author: Igor Maculan - n3wtron@gmail.com
+	A Simple mjpg stream http server
+'''
+capture = None
+
+
+class CamHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path.endswith('.mjpg'):
+            print("HEEEEEEEEEEEEEEEEEEEEEEEEY")
+            self.send_response(200)
+            self.send_header(
+                'Content-type', 'multipart/x-mixed-replace; boundary=--jpgboundary')
+            self.end_headers()
+            while True:
+                try:
+                    position = camera.getBallPosition()
+                    position = camera.trackBall(position)
+                    img = camera.getFrame()
+                    jpg = Image.fromarray(img)
+                    tmpFile = BytesIO()
+                    jpg.save(tmpFile,'JPEG')
+                    self.wfile.write("--jpgboundary".encode())
+                    self.send_header('Content-type','image/jpeg')
+                    self.send_header('Content-length',str(tmpFile.getbuffer().nbytes))
+                    self.end_headers()
+                    jpg.save(self.wfile,'JPEG')
+                    time.sleep(0.05)
+                except KeyboardInterrupt:
+                    break
+            return
+        if self.path.endswith('.html'):
+            self.send_response(200)
+            self.send_header('Content-type','text/html')
+            self.end_headers()
+            self.wfile.write('<html><head></head><body>'.encode())
+            self.wfile.write('<img src="http://127.0.0.1:8087/cam.mjpg"/>'.encode())
+            self.wfile.write('</body></html>'.encode())
+            return
+
+
 
 
 # while True:
@@ -36,50 +87,6 @@ def createCamera():
 # camera.startCapture()
 
 # with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
-
-'''
-	Author: Igor Maculan - n3wtron@gmail.com
-	A Simple mjpg stream http server
-'''
-import cv2
-from PIL import Image
-import threading
-from http.server import BaseHTTPRequestHandler,HTTPServer
-from socketserver import ThreadingMixIn
-from io import StringIO,BytesIO
-import time
-capture=None
-
-class CamHandler(BaseHTTPRequestHandler):
-	def do_GET(self):
-		if self.path.endswith('.mjpg'):
-			print("HEEEEEEEEEEEEEEEEEEEEEEEEY")
-			self.send_response(200)
-			self.send_header('Content-type','multipart/x-mixed-replace; boundary=--jpgboundary')
-			self.end_headers()
-			while True:
-				try:
-					img = camera.getFrame()
-					jpg = Image.fromarray(img)
-					tmpFile = BytesIO()
-					jpg.save(tmpFile,'JPEG')
-					self.wfile.write("--jpgboundary".encode())
-					self.send_header('Content-type','image/jpeg')
-					self.send_header('Content-length',str(tmpFile.getbuffer().nbytes))
-					self.end_headers()
-					jpg.save(self.wfile,'JPEG')
-					time.sleep(0.05)
-				except KeyboardInterrupt:
-					break
-			return
-		if self.path.endswith('.html'):
-			self.send_response(200)
-			self.send_header('Content-type','text/html')
-			self.end_headers()
-			self.wfile.write('<html><head></head><body>'.encode())
-			self.wfile.write('<img src="http://127.0.0.1:8087/cam.mjpg"/>'.encode())
-			self.wfile.write('</body></html>'.encode())
-			return
 
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
